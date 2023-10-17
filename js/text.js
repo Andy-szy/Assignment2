2e723892aae9728e6b3a77cec191e8b82452d866
zsuu0015 <zsuu0015@student.monash.edu>
2023-10-17 22:24:13 +1100

    modify data

--- js/text.js
+++ js/text.js
@@ -3,6 +3,11 @@ let participation_times_2 = document.getElementById("participation_times_2")
 let host_city = document.getElementById("host_city")
 let medal_distribution = document.getElementById("medal_distribution")
 let weight = document.getElementById("weight")
+let height = document.getElementById("height")
+let sports_item_1 = document.getElementById("sports_item_1")
+let sports_item_2 = document.getElementById("sports_item_2")
+let athlete_1 = document.getElementById("athlete_1")
+let athlete_2 = document.getElementById("athlete_2")
 
 participation_times_1.innerHTML = "From the graph, we can see that the relationship between Gold and " +
   "medal tally is a positive correlation relationship. When a country has more medal tally, by the increase" +
@@ -15,13 +20,13 @@ participation_times_2.innerHTML = "In addition, we can see that most of teams ha
   "<span style=\"color: #e41a1c;\">the United States</span> which attended " +
   "<span class='under_line' \">28</span> games and " +
   "<span style=\"color: #ff7f00;\">the United Kingdom</span> which attended " +
-  "<span class='under_line' \">29</span> games"
+  "<span class='under_line' \">29</span> games."
 
 host_city.innerHTML = "From this map, we can see most of counties does not host any olympic games. The reason is" +
   "to host a olympics game needing a lot of expense not only money but also human resources and " +
   "transportation. We can see that some cities like <span style=\"color: #e41a1c;\">" +
   "Los Angeles, London and Paris </span> have hosted <span class='under_line' \"> 3 times </span>" +
-  "of Olympics summer games. With the economics difficulty, the rich countries would have more change to hold" +
+  "of Olympics summer games. With the economics difficulty, the rich countries would have more change to hold." +
   "the olympic games"
 
 medal_distribution.innerHTML = "With the increase the attendance with more countries, more and more countries are more" +
@@ -29,7 +34,7 @@ medal_distribution.innerHTML = "With the increase the attendance with more count
   "the most models <span class=\'under_line\' style=\"color: #e41a1c;\">285</span> and China does not attend that game. " +
   "the United States have <span class=\'under_line\' style=\"color: #ec815c;\">155</span> medals and Australia have " +
   "<span class=\'under_line\' style=\"color: #f1af90;\">24</span> medals. With time goes on, China have more chance" +
-  " to have medals and in 2008, they have <span class=\'under_line\' style=\"color: #EA7651;\">184</span> medals in totol"
+  " to have medals and in 2008, they have <span class=\'under_line\' style=\"color: #EA7651;\">184</span> medals in totol."
 
 weight.innerHTML = "From the picture, we can see that people in the " +
   "<span class='under_line'>50-100kg</span> range have won the most MEDALS, " +
@@ -37,3 +42,36 @@ weight.innerHTML = "From the picture, we can see that people in the " +
   "<span class='under_line'>300</span> MEDALS. In the middle, the distribution of gold, silver and bronze " +
   "MEDALS is more evenly distributed, indicating that there is a certain weight requirement in a specific sport, " +
   "such as weightlifting or gymnastics."
+
+height.innerHTML = "This is also happened on height, <span class='under_line'>160-200cm</span> range have won " +
+  "the most MEDALS, of which <span class='under_line'>180cm</span> people have won more than " +
+  "<span class='under_line'>300</span> MEDALS. The distribution of gold, silver and bronze are also distributed" +
+  "evenly, the same reason as different sports have different enquirement of heights. "
+
+sports_item_1.innerHTML = "This chart shows how many MEDALS each sport will produce at each Olympic Games. " +
+  "It includes 32 categories. It shows in detail the number of specific events in each sport category from 1948 to " +
+  "the present. Of these, only sports that will still be played in 2024 are counted." +
+  "From this chart, we can see that Aquatics and Athletics have the most sports" +
+  "items from 1948 to 2024. In 2024, Aquatics has <span style=\"color: #e41a1c;\">49</span> " +
+  "items and Atheletics has <span style=\"color: #e41a1c;\">48</span> items."
+
+sports_item_2.innerHTML = "We can see that more and more sports are joined into the Olympics games. In 1948, there" +
+  "are only few sports have items. <span class='under_line' \">Archery, Badminton, Breaking, Golf, " +
+  "Handball, Judo, Skateboarding, Sport CLimbing, Surfing, Table Tennis, Taekwondo, Tennis, Triathlon " +
+  "and vallyball</span> did not even have any medals in this Year. In recent years, an increasing number of " +
+  "sports are joined to the Olympics and more athletes can show their different types of skills."
+
+athlete_1.innerHTML = "This graph shows the relationship of athletes number from 1948 to 2016 including" +
+  "summer and winter olympic games. There are two different colors. The blue color represents the " +
+  "<span style=\"color: #FFC0CB;\">male</span> athletes " +
+  "and the pink color represents the <span style=\"color: #87CEEB;\">female</span> " +
+  "athletes. From the first graph, we can see that <span style=\"color: #FFC0CB;\">male</span> athletes are more than " +
+  "<span style=\"color: #87CEEB;\">female</span> athletes from 1948 to 2016 including " +
+  "summer and winter Olympic games. However, more and more " +
+  "<span style=\"color: #87CEEB;\">female</span> are " +
+  "joining to the Olympic games and the <span style=\"color: #FFC0CB;\">male</span> athletes' number is fluctuated. " +
+  "Also, the number of winter olympic games is always smaller than that of summer"
+
+athlete_2.innerHTML = "The bottom graph shows the number of ages in the olympic games. You can drag the graph to filter" +
+  " the information. From the table, we can see the <span class='under_line'>23 and 24</span> years old athletes are the most and after that age," +
+  "the number is dropping down"
